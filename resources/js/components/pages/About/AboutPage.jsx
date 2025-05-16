ba4dd0ffce4b94bbfd47631b80e701dc07898587
pamel0h <sweetie11pamelo@gmail.com>
2025-05-16 23:09:55 +0300

    About page layout

--- resources/js/components/pages/About/AboutPage.jsx
+++ resources/js/components/pages/About/AboutPage.jsx
@@ -1,8 +1,25 @@
+import '../../../../css/components/AboutPage.css'; 
 
 const AboutPage = () => {
     return (
-        <div>
-            <h1>About us</h1>
+        <div className="page page--about">
+            <h1 className='titleAbout'>About us</h1>
+            <div className='imageAbout'>
+                <img src='/images/mainphoto.jpg'></img>
+            </div>
+            <h1 className='titleAbout1'>music for everyone</h1>
+            <div className='text1'></div>
+            <div className='text2'></div>
+            <h1 className='titleAbout2'>With you for more than 10 years</h1>
+            <div className='imageAbout1'></div>
+            <div className='imageAbout2'></div>
+            <h1 className='titleAbout3'>follow us on our social networks</h1>
+            <div className='media1'></div>
+            <div className='media2'></div>
+            <div className='media3'></div>
+            <p className='sign'>Clef 2025 ❤️</p>
+
+
         </div>
     );
 };
