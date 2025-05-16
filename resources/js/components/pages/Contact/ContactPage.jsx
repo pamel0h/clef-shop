f8c3650aac7deab924dcca84ac5ff128a4cd5745
pamel0h <sweetie11pamelo@gmail.com>
2025-05-16 23:41:27 +0300

    structure Contact page

--- resources/js/components/pages/Contact/ContactPage.jsx
+++ resources/js/components/pages/Contact/ContactPage.jsx
@@ -1,9 +1,24 @@
-
+import '../../../../css/components/ContactPage.css'; 
 
 const ContactPage = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className='page page--contact'>
+            <h1 className='titleContact'>Contacts</h1>
+            <h2>Our phone numbers</h2>
+            <div className='numbers'>
+                <ul>
+                    <li>+79780453769</li>
+                    <li>+79785901565</li>
+                </ul>
+                <p>Reception by phone from 10 am to 7 pm</p>
+            </div>
+            
+            <h2>How to get to us?</h2>
+            <div className='cardShop'></div>
+            <p>We are located at the address .... You need to go... blah blah</p>
+            <h2>Still have questions?</h2>
+            <p>Fill out the feedback form!</p>
+            <div className='form'></div>
         </div>
     );
 };
