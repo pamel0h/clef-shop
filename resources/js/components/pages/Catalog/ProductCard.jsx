b013c5a84ac51058f7ec14594a5c0246f0b40baf
pamelohhh <alenasderzhikovaa@gmail.com>
2025-05-22 00:21:06 +0300

    product cards + filter

--- resources/js/components/pages/Catalog/ProductCard.jsx
+++ resources/js/components/pages/Catalog/ProductCard.jsx
@@ -2,6 +2,7 @@
 import { Link } from 'react-router-dom';
 import ProductImage from './ProductImage';
 import ProductPrice from './ProductPrice';
+import Button from '../../UI/Button'
 import '../../../../css/components/Products.css';
 
 const ProductCard = ({ product, categorySlug, subcategorySlug }) => {
@@ -17,6 +18,7 @@ const ProductCard = ({ product, categorySlug, subcategorySlug }) => {
         <h3 className="product-name">{product.name}</h3>
         <ProductPrice price={product.price} discount={product.discount} />
       </Link>
+      <Button>В корзину</Button>
     </div>
   );
 };
