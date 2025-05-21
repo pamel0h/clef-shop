4433ef92ee8e5dbc3d33f3b67887c2b76fd68c0a
RingletN <nastasya-2004@mail.ru>
2025-05-21 22:09:42 +0300

    add page to div

--- resources/js/components/pages/Catalog/SearchPage.jsx
+++ resources/js/components/pages/Catalog/SearchPage.jsx
@@ -7,9 +7,9 @@ function SearchPage() {
     const query = queryParams.get('query') || '';
 
     return (
-        <div className="search-page">
+        <div className="search-page page">
             <h1>Search</h1>
-            <h1>Результаты поиска по запросу: {query || 'Ничего не введено'}</h1>
+            <h2>Результаты поиска по запросу: {query || 'Ничего не введено'}</h2>
             <p>{query ? `Вы ввели: ${query}` : 'Пожалуйста, введите запрос для поиска.'}</p>
             <div>
                 {query ? (
