cd4e6dee8b5d01789a2a128488a30dd7aa8d386a
RingletN <nastasya-2004@mail.ru>
2025-06-04 12:53:46 +0300

    add cart button

--- resources/js/components/pages/Catalog/ProductInfo.jsx
+++ resources/js/components/pages/Catalog/ProductInfo.jsx
@@ -1,11 +1,18 @@
 import { useTranslation } from 'react-i18next';
 import ProductPrice from './ProductPrice';
+import { useCart } from '../../../../context/CartContext'; // Импортируем useCart
+import Button from '../../UI/Button';
 
 const ProductInfo = ({ name, price, discount, brand, description }) => {
   const { t, i18n } = useTranslation();
+  const { addToCart } = useCart(); // Получаем функцию addToCart
 
-  console.log('ProductInfo: Props', { name, price, discount, brand, description });
-  console.log('ProductInfo: Current locale', i18n.language);
+  const handleAddToCart = async () => {
+    const result = await addToCart(product.id, 1); // Добавляем 1 товар
+    if (!result.success) {
+        console.error(result.error);
+    }
+};
 
   // Выбираем описание на основе текущего локейла
   const currentDescription = description?.[i18n.language] || description?.ru || description?.en || t('description_unavailable');
@@ -14,9 +21,13 @@ const ProductInfo = ({ name, price, discount, brand, description }) => {
     <div className="product-info">
       <h1 className="product-name">{name || t('no_name')}</h1>
       <ProductPrice price={price} discount={discount} />
+      <Button onClick={handleAddToCart}>
+                {t('cart.add_to_cart')}
+      </Button>
       <div className="product-description">
         <p>
-          <strong>{t('description')}:</strong> {currentDescription}
+          {/* <strong>{t('description')}:</strong>  */}
+          {currentDescription}
         </p>
       </div>
       <p className="product-brand">
